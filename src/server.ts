e8209e06ec502f44e994b4444330404a94d70c91
just9krish <rvamit2648@gmail.com>
2023-03-16 16:01:12 +0530

    initial commit

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,32 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
 import express from "express";
+import fs from "fs";
+import path from "path";
+import mongoose from "mongoose";
+import morgan from "morgan";
 
 const app = express();
-const ads = "Sfsfas";
+
+// middleware
+app.use(morgan("combined"));
+app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("hellow owrd");
 });
 
-app.listen(3000, () => console.log("server is listening"));
+mongoose
+  .connect(process.env.DBPATH || "mongodb://localhost/test")
+  .then(() =>
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is listening to port ${process.env.PORT}`);
+    })
+  )
+  .catch((err) => {
+    console.log("error for starting the database" + err);
+  });
